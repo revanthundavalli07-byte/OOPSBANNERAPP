0c77fad2114366289254f155d442f6bd5b3154fe
revanthundavalli07-byte <revanthundavalli07@gmail.com>
2026-03-14 17:45:22 +0530

    uc7 OOPS IMPLEMENTATION

---
+++ CharacterPattern.java
@@ -0,0 +1,14 @@
+public class CharacterPattern {
+
+    char letter;
+    String[] pattern;
+
+    public CharacterPattern(char letter, String[] pattern) {
+        this.letter = letter;
+        this.pattern = pattern;
+    }
+
+    public String[] getPattern() {
+        return pattern;
+    }
+}
