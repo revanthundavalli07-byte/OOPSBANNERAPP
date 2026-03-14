e0a516a6b9e45361d67d49d2ef0f3bec566c382c
revanthundavalli07-byte <revanthundavalli07@gmail.com>
2026-03-14 17:33:01 +0530

    OOPS BANNER USING ARRAY INITILISATION

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,11 +1,19 @@
-class OOPSBANNERAPP{
-public static void main(String args[]){
-System.out.println("OOPS");
+public class OOPSBANNERAPP {
 
+    public static void main(String[] args) {
 
+        String[] banner = {
+            " *****    *****    ******    ****** ",
+            "*     *  *     *   *     *   *      ",
+            "*     *  *     *   *     *   *      ",
+            "*     *  *     *   ******    ****** ",
+            "*     *  *     *   *         *     *",
+            "*     *  *     *   *         *     *",
+            " *****    *****    *          ******"
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
 }
-
-}
-
-
-
