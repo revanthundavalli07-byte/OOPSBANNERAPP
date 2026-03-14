9458f6ca40c354f446c0a4a5a332126665da86a6
revanthundavalli07-byte <revanthundavalli07@gmail.com>
2026-03-14 17:15:47 +0530

    Change output to display a decorative banner
    Updated the OOPSBANNERAPP class to print a banner instead of 'OOPS'.

--- OOPSBANNERAPP.java
+++ OOPSBANNERAPP.java
@@ -1,11 +1,14 @@
-class OOPSBANNERAPP{
-public static void main(String args[]){
-System.out.println("OOPS");
+class OOPSBANNERAPP {
+    public static void main(String[] args) {
 
+        System.out.println(" *****   *****   *****   ***** ");
+        System.out.println("*     * *     * *     * *     *");
+        System.out.println("*     * *     * *     * *      ");
+        System.out.println("*     * *     * *****    ***** ");
+        System.out.println("*     * *     * *              *");
+        System.out.println("*     * *     * *        *     *");
+        System.out.println(" *****   *****  *         ***** ");
 
+    }
 }
-
-}
-
-
 
